dbd5af7c2b73dcd49e03896365665998358d83b1
Alexander Forselius <alecca@Alexanders-iMac.lan>
2016-11-11 20:59:27 +0100

    T

--- Aquafulness/AppDelegate.swift
+++ Aquafulness/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UIApplication.shared.keyWindow?.tintColor = UIColor(red: 0, green: 0, blue: 255, alpha: 1);
+        
         return true
     }
 
