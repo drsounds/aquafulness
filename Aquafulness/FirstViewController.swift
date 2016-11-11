11786b8f46e7275497bde11fc4e7a045e92a6f34
Alexander Forselius <alecca@alexanders-imac.lan>
2016-11-11 19:46:54 +0100

    Initial Commit

---
+++ Aquafulness/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Aquafulness
+//
+//  Created by Alexander Forselius on 2016-11-11.
+//  Copyright © 2016 Alexander Forselius. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Aquafulness/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Aquafulness
+//
+//  Created by Alexander Forselius on 2016-11-11.
+//  Copyright © 2016 Alexander Forselius. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
